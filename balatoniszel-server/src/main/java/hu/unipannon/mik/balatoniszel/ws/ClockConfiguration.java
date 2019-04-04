25aefa9ca9fe39f7730a1fa7534bdb0b0cc1a6a3
Bálint Persics <b.persics@mortoff.hu>
2019-04-04 19:00:29 +0200

    Advanced deposit handling.

---
+++ balatoniszel-server/src/main/java/hu/unipannon/mik/balatoniszel/ws/ClockConfiguration.java
@@ -0,0 +1,15 @@
+package hu.unipannon.mik.balatoniszel.ws;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class ClockConfiguration {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
+}
